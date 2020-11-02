94edd35f3be5c7486250606e88d19c006ced57e4
Wiktor Artymowicz <wiktor.artymowicz44@gmail.com>
2020-11-02 21:44:48 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTutorial
+{
+    public class Person
+    {
+        private string _name = string.Empty;
+        private int _age = 0;
+
+        public string Name { get => _name; set => _name = value; }
+        public int Age { get => _age; set => _age = value; }
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public Person()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Imię: {_name}, Wiek: {_age}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTutorial
+{
+    class Program
+    {
+        public delegate bool FunctionDelegate(Person person);
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person() { Name = "Wiktor", Age = 28 });
+            people.Add(new Person() { Name = "Anna", Age = 30 });
+            people.Add(new Person() { Name = "Adam", Age = 5 });
+
+            FunctionDelegate functionDelegate = new FunctionDelegate(TestBool);
+
+            GetOlder(people, x => x.Age <= 20);
+
+            Console.ReadLine();
+        }
+
+        public static void GetOlder(List<Person> people, FunctionDelegate functionDelegate)
+        {
+            
+            foreach (var item in people)
+            {
+                if (functionDelegate(item))
+                {
+                    item.Age++;
+                    Console.WriteLine(item.ToString());
+                }
+            }
+        }
+
+        public static bool TestBool(Person person)
+        {
+            if (person.Age > 10)
+                return true;
+            else
+                return false;
+        }
+    }
+
+}
